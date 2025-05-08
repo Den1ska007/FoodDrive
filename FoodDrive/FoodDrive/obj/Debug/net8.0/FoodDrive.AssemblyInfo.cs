dad146a140d00f06dbc87ed0581b5903c42737fe
Anatoliy Burdak <byrdak16@gmail.com>
2025-05-08 16:41:56 +0300

    Works

--- FoodDrive/FoodDrive/obj/Debug/net8.0/FoodDrive.AssemblyInfo.cs
+++ FoodDrive/FoodDrive/obj/Debug/net8.0/FoodDrive.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodDrive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e09772325734aea7fb6c517f2afb643b613693")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24f629aa69036c1d5dccc010e9d51f4184e6e3f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodDrive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodDrive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
