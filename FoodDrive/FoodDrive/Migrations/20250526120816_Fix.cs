167e9e47bad50eed420d9b5031027a4ab70f01f5
Anatoliy Burdak <byrdak16@gmail.com>
2025-05-26 15:22:30 +0300

    Fix

---
+++ FoodDrive/FoodDrive/Migrations/20250526120816_Fix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodDrive.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "balance",
+                table: "Customers",
+                newName: "Balance");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Balance",
+                table: "Customers",
+                newName: "balance");
+        }
+    }
+}
--- FoodDrive/FoodDrive/obj/Debug/net8.0/FoodDrive.AssemblyInfo.cs
+++ FoodDrive/FoodDrive/obj/Debug/net8.0/FoodDrive.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodDrive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f990cdd70d8cb5a3029a9d975b1104d84df795")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1549f2d4e4654555bf2ce34d8ae36fb9f43b7485")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodDrive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodDrive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
